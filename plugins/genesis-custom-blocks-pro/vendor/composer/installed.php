cefbe1feb29c6699c36e173feee1ca1fbc1f95de
Orases Dev <dev@orases.com>
2024-04-03 14:29:57 -0400

    Initial commit

---
+++ plugins/genesis-custom-blocks-pro/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.7.0',
+        'version' => '1.7.0.0',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '4f2297bb30645807999694e157a8429e665f6f18',
+        'name' => 'studiopress/genesis-custom-blocks-pro',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'studiopress/genesis-custom-blocks' => array(
+            'pretty_version' => 'trunk',
+            'version' => 'dev-trunk',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../studiopress/genesis-custom-blocks',
+            'aliases' => array(),
+            'reference' => 'trunk',
+            'dev_requirement' => false,
+        ),
+        'studiopress/genesis-custom-blocks-pro' => array(
+            'pretty_version' => '1.7.0',
+            'version' => '1.7.0.0',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '4f2297bb30645807999694e157a8429e665f6f18',
+            'dev_requirement' => false,
+        ),
+    ),
+);
